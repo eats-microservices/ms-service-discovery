739239354181cd9ad50068981b7bd2a9b4d826cf
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-10-23 21:31:22 -0300

    Initial commit

---
+++ src/main/java/com/eats/msservicediscovery/MsServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.eats.msservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eats/msservicediscovery/MsServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eats.msservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
