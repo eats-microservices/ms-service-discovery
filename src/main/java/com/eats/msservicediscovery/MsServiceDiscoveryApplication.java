50cbae4e840dc412b178974831160bb2951e9dde
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-11-21 10:19:50 -0300

    feat: changed properties to yml structure

--- src/main/java/com/eats/msservicediscovery/MsServiceDiscoveryApplication.java
+++ src/main/java/com/eats/msservicediscovery/MsServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.eats.msservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
